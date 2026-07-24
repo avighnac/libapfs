b1008b3f3c1dfcaba0007e210041511f490798b8
SYCKGit <oviyangandhi@gmail.com>
2026-07-25 05:13:57 +0530

    FIx missing parsed resize in parse_j_btree_node, move comments to bottom and g++ fixes
    Co-authored-by: Avighna Chhatrapati <avighnac@users.noreply.github.com>

--- test.cpp
+++ test.cpp
@@ -1,109 +1,11 @@
-// #include "src/guid.hpp"
-// #include "src/types.hpp"
-// #include <cassert>
-// #include <cstring>
-// #include <fcntl.h>
-// #include <iostream>
-// #include <sys/disk.h>
-// #include <sys/ioctl.h>
-// #include <unistd.h>
-// #include <vector>
-
-// int main() {
-//   // /dev/rdisk5
-
-//   FILE *f = fopen("/dev/rdisk5", "rb");
-//   if (!f) {
-//     std::cout << "maybe insufficient perms?\n";
-//     return 1;
-//   }
-
-//   int BLOCK_SIZE;
-//   if (ioctl(fileno(f), DKIOCGETBLOCKSIZE, &BLOCK_SIZE) == 0) {
-//     std::cout << "using block size = " << BLOCK_SIZE << '\n';
-//   } else {
-//     std::cout << "block size failed\n";
-//     return 1;
-//   }
-
-//   std::string sector_buff(BLOCK_SIZE, 0);
-
-//   if (fread((char *)sector_buff.data(), BLOCK_SIZE, 1, f) <= 0) {
-//     std::cout << "read mbr failed\n";
-//     return 1;
-//   }
-//   MASTER_BOOT_RECORD mbr = *(MASTER_BOOT_RECORD *)sector_buff.data();
-//   assert(mbr.Signature == MBR_SIGNATURE);
-
-//   if (fread((char *)sector_buff.data(), BLOCK_SIZE, 1, f) <= 0) {
-//     std::cout << "read gpt header failed\n";
-//     return 1;
-//   }
-//   GPT_HEADER header = *(GPT_HEADER *)sector_buff.data();
-//   assert(header.Signature == GPT_HEADER_SIGNATURE);
-
-//   std::cout << "number of partition entries: " << header.NumberOfPartitionEntries << '\n';
-//   std::cout << "size of each partition entry: " << header.SizeOfPartitionEntry << '\n';
-
-//   // move file to header.PartitionEntryLBA
-//   if (fseek(f, header.PartitionEntryLBA * BLOCK_SIZE, SEEK_SET) == -1) {
-//     std::cout << "lseek failed\n";
-//     return 1;
-//   }
-
-//   std::vector<EFI_PARTITION_ENTRY> partitions(header.NumberOfPartitionEntries);
-//   if (fread((char *)partitions.data(), header.SizeOfPartitionEntry, header.NumberOfPartitionEntries, f) < header.NumberOfPartitionEntries) {
-//     std::cout << "read partition entries failed\n";
-//     return 1;
-//   }
-
-//   int64_t addr = 0;
-//   for (EFI_PARTITION_ENTRY &part : partitions) {
-//     if (memcmp(part.PartitionTypeGUID, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 16) == 0) {
-//       continue;
-//     }
-//     // skip the efi system partititon
-//     if (memcmp(part.PartitionTypeGUID, "\x28\x73\x2a\xc1\x1f\xf8\xd2\x11\xba\x4b\x00\xa0\xc9\x3e\xc9\x3b", 16) == 0) {
-//       continue;
-//     }
-//     std::cout << "using partititon with type guid ";
-//     for (int i = 0; i < 16; ++i) {
-//       std::cout << std::hex << (int)part.PartitionTypeGUID[i] << ' ';
-//     }
-//     std::cout << '\n';
-//     addr = part.StartingLBA * BLOCK_SIZE;
-//   }
-
-//   std::cout << std::dec << addr << '\n';
-
-//   // seek to the start of the apfs container
-//   if (fseek(f, addr, SEEK_SET) == -1) {
-//     std::cout << "lseek to apfs failed\n";
-//     return 1;
-//   }
-
-//   std::cout << "---\n";
-//   nx_superblock_t superblock;
-//   if (fread(&superblock, sizeof(superblock), 1, f) <= 0) {
-//     std::cout << "superblock read failed\n";
-//     return 1;
-//   }
-
-//   std::cout << superblock.nx_xp_data_base << '\n';
-//   if ((superblock.nx_xp_data_base >> 63) & 1) {
-//     std::cout << "highest bit set, so it's a b-tree thing, unimplemented\n";
-//     return 1;
-//   }
-
-//   fclose(f);
-// }
-
 #include "src/checksum.hpp"
 #include "src/types.hpp"
+#include <algorithm>
 #include <cassert>
 #include <cstdio>
 #include <iomanip>
 #include <iostream>
+#include <ranges>
 #include <vector>
 
 // A utility class that reads raw memory from blocks in chunks of BLOCK_SIZE
@@ -139,35 +41,34 @@ public:
     }
     return *(T *)mem.data();
   }
-
-  template <>
-  checkpoint_map_phys_t read_object(uint64_t block_num) {
-    std::string mem = read_block(block_num);
-    if (!verify_object_checksum((void *)mem.data(), BLOCK_SIZE)) {
-      throw std::runtime_error("checksum verification failed while reading object");
-    }
-    size_t off = sizeof(checkpoint_map_phys_t) - sizeof(std::vector<checkpoint_mapping_t>);
-    checkpoint_map_phys_t obj;
-    memcpy((void *)&obj, mem.data(), off);
-    obj.cpm_map.resize(obj.cpm_count);
-    memcpy(obj.cpm_map.data(), mem.data() + off, obj.cpm_count * sizeof(checkpoint_mapping_t));
-    return obj;
-  }
-
-  template <>
-  btree_node_phys_t read_object(uint64_t block_num) {
-    std::string mem = read_block(block_num);
-    if (!verify_object_checksum((void *)mem.data(), BLOCK_SIZE)) {
-      throw std::runtime_error("checksum verification failed while reading object");
-    }
-    size_t off = sizeof(btree_node_phys_t) - sizeof(std::vector<uint8_t>);
-    btree_node_phys_t obj;
-    memcpy((void *)&obj, mem.data(), off);
-    obj.btn_data.resize((BTREE_NODE_SIZE_DEFAULT - off) / sizeof(uint8_t));
-    memcpy(obj.btn_data.data(), mem.data() + off, BTREE_NODE_SIZE_DEFAULT - off);
-    return obj;
-  }
 };
+
+template <>
+checkpoint_map_phys_t BlockReader::read_object(uint64_t block_num) {
+  std::string mem = read_block(block_num);
+  if (!verify_object_checksum((void *)mem.data(), BLOCK_SIZE)) {
+    throw std::runtime_error("checksum verification failed while reading object");
+  }
+  size_t off = sizeof(checkpoint_map_phys_t) - sizeof(std::vector<checkpoint_mapping_t>);
+  checkpoint_map_phys_t obj;
+  memcpy((void *)&obj, mem.data(), off);
+  obj.cpm_map.resize(obj.cpm_count);
+  memcpy(obj.cpm_map.data(), mem.data() + off, obj.cpm_count * sizeof(checkpoint_mapping_t));
+  return obj;
+}
+
+template <>
+btree_node_phys_t BlockReader::read_object(uint64_t block_num) {
+  std::string mem = read_block(block_num);
+  if (!verify_object_checksum((void *)mem.data(), BLOCK_SIZE)) {
+    throw std::runtime_error("checksum verification failed while reading object");
+  }
+  size_t off = sizeof(btree_node_phys_t) - sizeof(std::string);
+  btree_node_phys_t obj;
+  memcpy((void *)&obj, mem.data(), off);
+  obj.btn_data.append(mem.data() + off, BTREE_NODE_SIZE_DEFAULT - off);
+  return obj;
+}
 
 // An entry in the checkpoint descriptor area.
 struct container_t {
@@ -354,6 +255,8 @@ j_btree_node parse_j_btree_node(const btree_node_phys_t &node) {
 
   // There are `node.btn_nkeys` key-value pairs
   j_btree_node parsed;
+  parsed.keys.resize(node.btn_nkeys);
+  parsed.vals.resize(node.btn_nkeys);
   for (int i = 0; i < node.btn_nkeys; ++i) {
     kvloc_t loc = *(kvloc_t *)(table_loc + i * sizeof(kvloc_t));
     parsed.keys[i] = read_j_key_t(keys_loc, loc.k);
@@ -385,7 +288,7 @@ int main() {
       container.block = *(nx_superblock_t *)data.data();
 
       // Only process if the checksum of the superblock is valid.
-      if (!verify_object_checksum(data.data(), superblock.nx_block_size)) {
+      if (!verify_object_checksum((void *)data.data(), superblock.nx_block_size)) {
         continue;
       }
 
@@ -430,7 +333,110 @@ int main() {
   btree_node_phys_t fs_tree = reader.read_object<btree_node_phys_t>(apfs_root_tree_addr);
   j_btree_node j_node = parse_j_btree_node(fs_tree);
   std::cout << "keys\n";
-  for (auto k : j_node.keys) {
-    std::cout << std::hex << k.obj_id_and_type << std::endl;
+  for (auto [k, v] : std::views::zip(j_node.keys, j_node.vals)) {
+    uint64_t key_type = (k.obj_id_and_type & OBJ_TYPE_MASK) >> OBJ_TYPE_SHIFT;
+    if (key_type == APFS_TYPE_DIR_REC) {
+      std::cout << (*(j_drec_hashed_key_t *)&k).name_len_and_hash << ' ' << (*(j_drec_hashed_key_t *)&k).name << std::endl;
+    }
   }
 }
+
+// #include "src/guid.hpp"
+// #include "src/types.hpp"
+// #include <cassert>
+// #include <cstring>
+// #include <fcntl.h>
+// #include <iostream>
+// #include <sys/disk.h>
+// #include <sys/ioctl.h>
+// #include <unistd.h>
+// #include <vector>
+
+// int main() {
+//   // /dev/rdisk5
+
+//   FILE *f = fopen("/dev/rdisk5", "rb");
+//   if (!f) {
+//     std::cout << "maybe insufficient perms?\n";
+//     return 1;
+//   }
+
+//   int BLOCK_SIZE;
+//   if (ioctl(fileno(f), DKIOCGETBLOCKSIZE, &BLOCK_SIZE) == 0) {
+//     std::cout << "using block size = " << BLOCK_SIZE << '\n';
+//   } else {
+//     std::cout << "block size failed\n";
+//     return 1;
+//   }
+
+//   std::string sector_buff(BLOCK_SIZE, 0);
+
+//   if (fread((char *)sector_buff.data(), BLOCK_SIZE, 1, f) <= 0) {
+//     std::cout << "read mbr failed\n";
+//     return 1;
+//   }
+//   MASTER_BOOT_RECORD mbr = *(MASTER_BOOT_RECORD *)sector_buff.data();
+//   assert(mbr.Signature == MBR_SIGNATURE);
+
+//   if (fread((char *)sector_buff.data(), BLOCK_SIZE, 1, f) <= 0) {
+//     std::cout << "read gpt header failed\n";
+//     return 1;
+//   }
+//   GPT_HEADER header = *(GPT_HEADER *)sector_buff.data();
+//   assert(header.Signature == GPT_HEADER_SIGNATURE);
+
+//   std::cout << "number of partition entries: " << header.NumberOfPartitionEntries << '\n';
+//   std::cout << "size of each partition entry: " << header.SizeOfPartitionEntry << '\n';
+
+//   // move file to header.PartitionEntryLBA
+//   if (fseek(f, header.PartitionEntryLBA * BLOCK_SIZE, SEEK_SET) == -1) {
+//     std::cout << "lseek failed\n";
+//     return 1;
+//   }
+
+//   std::vector<EFI_PARTITION_ENTRY> partitions(header.NumberOfPartitionEntries);
+//   if (fread((char *)partitions.data(), header.SizeOfPartitionEntry, header.NumberOfPartitionEntries, f) < header.NumberOfPartitionEntries) {
+//     std::cout << "read partition entries failed\n";
+//     return 1;
+//   }
+
+//   int64_t addr = 0;
+//   for (EFI_PARTITION_ENTRY &part : partitions) {
+//     if (memcmp(part.PartitionTypeGUID, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 16) == 0) {
+//       continue;
+//     }
+//     // skip the efi system partititon
+//     if (memcmp(part.PartitionTypeGUID, "\x28\x73\x2a\xc1\x1f\xf8\xd2\x11\xba\x4b\x00\xa0\xc9\x3e\xc9\x3b", 16) == 0) {
+//       continue;
+//     }
+//     std::cout << "using partititon with type guid ";
+//     for (int i = 0; i < 16; ++i) {
+//       std::cout << std::hex << (int)part.PartitionTypeGUID[i] << ' ';
+//     }
+//     std::cout << '\n';
+//     addr = part.StartingLBA * BLOCK_SIZE;
+//   }
+
+//   std::cout << std::dec << addr << '\n';
+
+//   // seek to the start of the apfs container
+//   if (fseek(f, addr, SEEK_SET) == -1) {
+//     std::cout << "lseek to apfs failed\n";
+//     return 1;
+//   }
+
+//   std::cout << "---\n";
+//   nx_superblock_t superblock;
+//   if (fread(&superblock, sizeof(superblock), 1, f) <= 0) {
+//     std::cout << "superblock read failed\n";
+//     return 1;
+//   }
+
+//   std::cout << superblock.nx_xp_data_base << '\n';
+//   if ((superblock.nx_xp_data_base >> 63) & 1) {
+//     std::cout << "highest bit set, so it's a b-tree thing, unimplemented\n";
+//     return 1;
+//   }
+
+//   fclose(f);
+// }
